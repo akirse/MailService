4511b94a37aea2b7718f6d1f04d6e29cb7d66557
andy <andy@DesktopWin.fritz.box>
2016-07-17 19:50:20 +0200

    Hi

--- helloworld.py
+++ helloworld.py
@@ -9,7 +9,7 @@ class MainPage(webapp.RequestHandler):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.out.write('Hello, webapp World!')
         
-        self.response.out.write('<p>Yeah 2</p>')
+        self.response.out.write('<p>Yeah 3</p>')
 
 
 application = webapp.WSGIApplication([('/', MainPage)], debug=True)
